f456578a42b93a9b621fb8ebf8ecd32bf06fb40d
codeclassifiers <codeclassifiers@gmail.com>
2020-09-07 08:11:26 +0530

    1st commit

---
+++ js/index.js
@@ -0,0 +1,19 @@
+/**
+ * Function to populate values in card from form input
+ */
+function generateCard() {
+    // Get value of Student name from form input 
+    
+    // Assign the value of student name to generated card
+    
+    // Get value of college name from form input 
+    
+    // Assign the value of college name to generated card
+    
+    // Get value of location name from form input 
+    
+    // Assign the value of location name to generated card
+    
+    // Display final generated card to user     
+    document.getElementById("collegeCard").style.display = "block";
+}
